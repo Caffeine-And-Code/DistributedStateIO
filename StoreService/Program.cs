2ad78bf7dcd9e48e5d95607ec7470b8d596f6d0b
fabio <fabiofattoriw@gmail.com>
2025-12-17 03:28:42 +0100

    wow

--- StoreService/Program.cs
+++ StoreService/Program.cs
@@ -6,8 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-builder.Services.AddScoped<Class1>();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
